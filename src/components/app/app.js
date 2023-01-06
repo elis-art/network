023a8a2b45906f3b905e3f8b31807b1483ceab3c
Elvina Pavlovskaya <pavl.elvina@gmail.com>
2023-01-06 19:31:08 +0300

    added functionality to delete posts

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import PostStatusFilter from '../post-status-filter';
@@ -7,25 +7,44 @@ import PostAddForm from '../post-add-form';
 
 import './app.css';
 
+export default class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+    data : [
+        {label: 'Going to learn React', important: true, id: 'dfdd'},
+        {label: 'That is so good', important: false, id: 'dfhj'},
+        {label: 'I need a break...', important: false, id: 'dfh4'}
+    ]
+  };
+  this.deleteItem = this.deleteItem.bind(this);
+}
 
-const App = () => {
-  const data = [
-    {label: 'Going to learn React', important: true},
-    {label: 'That is so good', important: false},
-    {label: 'I need a break...', important: false}
-  ]
+deleteItem(id){
+  this.setState(({data}) => {
+    
+    const index = data.findIndex(elem => elem.id ===id );
+    const newArr = [...data.slice(0, index), ...data.slice(index + 1)];
 
-  return (
+    return {
+      data: newArr
+    }
+  })
+}
+
+render() {
+    return (
     <div className='App'>
       <AppHeader/>
       <div className='seach-panel d-flex'>
         <SearchPanel/>
         <PostStatusFilter/>
       </div>
-      <PostList posts={data}/>
+      <PostList 
+        posts={this.state.data}
+        onDelete={this.deleteItem}/>
       <PostAddForm/>
     </div>
-  )
+    )
+  }
 }
-
-export default App;
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,16 +1,16 @@
-import React from 'react';
-
+import React from 'react'; 
 import PostListItem from '../post-list-item';
 import './post-list.css'
 
-const PostList = ({posts}) => {
-
+const PostList = ({posts, onDelete}) => {
     const elements = posts.map( (item) => {
         if ( typeof item === 'object' && isEmpty(item) ){ 
             const {id, ...itemProps} = item;
             return (
                 <li key = {id} className='list-group-item'>
-                    <PostListItem {...itemProps}/>
+                    <PostListItem 
+                    {...itemProps} 
+                    onDelete={() => onDelete(id)}/>
                 </li>
             )
         }
