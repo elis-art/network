307be42968d0f67716ab9f1c1068ea505a59570a
Elvina Pavlovskaya <pavl.elvina@gmail.com>
2023-01-08 15:46:47 +0300

    Final work

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -2,31 +2,24 @@ import React from 'react';
 import PostListItem from '../post-list-item';
 import './post-list.css'
 
-const PostList = ({posts, onDelete}) => {
-    const elements = posts.map( (item) => {
-        if ( typeof item === 'object' && isEmpty(item) ){ 
-            const {id, ...itemProps} = item;
-            return (
-                <li key = {id} className='list-group-item'>
-                    <PostListItem 
-                    {...itemProps} 
-                    onDelete={() => onDelete(id)}/>
-                </li>
-            )
-        }
-    })
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
 
-    function isEmpty(obj) {
-        for(let key in obj)
-        {
-            return true;
-        }
-        return false;
-    }
+    const elements = posts.map((item) => {
+        const {id, ...itemProps} = item;
+        return (
+            <li key={id} className='list-group-item'>
+                <PostListItem 
+                    {...itemProps}
+                    onDelete={() => onDelete(id)}
+                    onToggleImportant={() => onToggleImportant(id)}
+                    onToggleLiked={() => onToggleLiked(id)}/>
+            </li>
+        )
+    });
 
     return (
         <ul className="app-list list-group">
-           {elements}
+            {elements}
         </ul>
     )
 }
